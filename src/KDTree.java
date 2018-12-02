055e66563c7d9248b03698c1ca2c11f0523e2f71
m1k1saur1o <dreamer.5122@gmail.com>
2018-12-02 15:21:46 -0300

    esqueleto de la clase

---
+++ src/KDTree.java
@@ -0,0 +1,36 @@
+/**
+ * Algoritmos y estructuras de datos 2018-2
+ */
+
+public class KDTree{
+	public static class Node{
+
+		}
+	public static class KdTree {
+		/**
+		 * Inserta un las coordenadas dadas en el KDtree
+		 * 
+		 */
+		public void insert(double x, double y){
+
+		}
+
+		/**
+		 * Retorna el nodo mas cercano a las coordenadas dadas
+		 * 
+		 */
+		public Node get(double x, double y){
+			return null;
+		}
+
+	}
+	/**
+	 * La implementacion debe recibir 2 archivos por la linea de argumentos
+	 * donde el primero es el arbol a construir y el segundo es el archivo
+	 * con consultas. Se deben imprimir los resultados de las consultas por
+	 * por la salida estandar.
+	 */
+	public static void main(String args[]){
+
+	}
+}
